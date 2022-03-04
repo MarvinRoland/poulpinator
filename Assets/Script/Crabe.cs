49a287cd51a48efa6cbefbd2677d458704209734
pampppp <59504030+pampppp@users.noreply.github.com>
2022-03-04 20:54:30 +0100

    player + crabe + scenne update

---
+++ Assets/Script/Crabe.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class Crabe : MonoBehaviour
+{
+    [SerializeField] float crabeSpeed; 
+    [SerializeField] int bulleNombreParAttaque;
+    [SerializeField] float bulleMinTaille, bulleMMaxTaille;
+    [SerializeField] float bulleSpeedMin, bulleSpeedMax;
+    [SerializeField] float bulleAngleDirection;
+    [SerializeField] float bulleAttaqueCD;
+    [SerializeField] float bulleTempsEntreChaqueBulle;
+    [SerializeField] GameObject bulleObject;
+
+
+    private bool isBulleAttaque;
+    private float savedTimeBulle, savedTimeBulleAttaque;
+    private int nbBulle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        savedTimeBulle = Time.time;
+        savedTimeBulleAttaque = Time.time;
+        isBulleAttaque = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        AttaqueBulle();
+        SetBulleAttaqueCD();
+        
+    }
+    public void AttaqueBulle()
+    {
+        if(Time.time > savedTimeBulle + bulleTempsEntreChaqueBulle + Random.Range(-0.1f,0.1f) && isBulleAttaque)
+        {
+            Vector3 startBullePosition = new Vector3(this.transform.position.x+Random.Range(-1,1), this.transform.position.y, this.transform.position.z);
+            GameObject bulle = Instantiate(bulleObject,startBullePosition,Quaternion.identity);
+            bulle.GetComponent<Bulle>().speed = Random.Range(bulleSpeedMin,bulleSpeedMax);
+            bulle.transform.localRotation = Quaternion.Euler(0,0,Random.Range(-bulleAngleDirection,bulleAngleDirection));
+            nbBulle += 1;
+            savedTimeBulle = Time.time;
+            if(nbBulle >= bulleNombreParAttaque)
+            {
+                isBulleAttaque = false;
+            }
+        }
+        
+    }
+    public void SetBulleAttaqueCD()
+    {
+        if (Time.time > savedTimeBulleAttaque + bulleAttaqueCD)
+        {
+            nbBulle = 0;
+            isBulleAttaque = true;
+            savedTimeBulleAttaque = Time.time;
+        }
+    }
+    public void Moove()
+    {
+        
+    }
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float playerPropulsionForce; // determine la la force de propulsion du poulpe (en gros la distance jusqu'ou ca le propulsera apres un input)
+    [SerializeField] float playerDeceleration; // la vitesse a la quel le poulpe passe de X vitesse a zero quand on ne se propulse pas
+    [SerializeField] float ancrePropulsionDistance; // la distance max jusqu'ou l'ancre serra propulsée
+    [SerializeField] float ancrePuissanceDePropulsion; // vitesse de depart du jet d'ancre 
+    [SerializeField] float ancrePuissanceDecroissement; // vitesse a la quel l'ancre passe de x vitesse a zero
+    [SerializeField] bool ancreIsToxic; // si TRUE l'ancre serra toxic et ferra des degats
+    [SerializeField] float ancreDegatsDirect, ancreDegatsDegatsParSeconde; // les degats que l'ancre infligera a l'impacte
+    [SerializeField] float ancreDureeEmpoisonement; // durée de l'empoisonement
+    [SerializeField] float ancreCD; // temps entre chaque jet d'ancre
+    [SerializeField] float ancreReserve; // reserve d'ancre
+    [SerializeField] float ancreRegenerationPassive; // ancre ajouté tout les x seconde sans manger de proie
+    [SerializeField] float ancreRegenerationActive; // ancre ajouté apres avoir mangé une proie
+    [SerializeField] float ancreReserveAjoutTime; // temps entre chaque ajout naturel d'ancre dans la reserve
+    [SerializeField] float rotationSpeed; // vitesse de rotation du poulpe
+    Vector3 v3Force;
+    
+
+
+
+    
+    private float z;
+    private Rigidbody rb;
+    
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {        
+        rb = this.gameObject.GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotation();
+        Propulsion();
+        if (rb.velocity.y < -1)
+        {
+            //rb.velocity = rb.velocity.normalized;
+        }
+        Debug.Log(rb.velocity);
+    }
+    void FixedUpdate()
+    {
+
+    }
+
+    public void Propulsion()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            v3Force = playerPropulsionForce * transform.up;
+            Debug.Log("propulse");
+            rb.AddForce(v3Force,ForceMode.Impulse);
+            //rb.AddForce(0, playerPropulsionForce, 0, ForceMode.Impulse);
+            //rb.AddForce(0, -playerDeceleration, 0, ForceMode.Impulse);
+        }
+        else
+        {
+            v3Force = playerDeceleration * -transform.up;
+            Debug.Log("propulse");
+            rb.AddForce(v3Force,ForceMode.Acceleration);
+        }
+    }
+    public void Rotation()
+    {
+        if (Input.GetButton("Horizontal"))
+        {
+            z += Time.deltaTime * rotationSpeed * Input.GetAxis("Horizontal");
+
+            if (z > 360.0f)
+            {
+                z = 0.0f;
+                
+            }
+            transform.localRotation = Quaternion.Euler(0, 0, z);
+        }
+    }
+    public void JetDancre()
+    {
+        if (Input.GetButtonDown("Fire"))
+        {
+
+        }
+    }
+    public void DragTentacle()
+    {
+
+    }
+    public void Die()
+    {
+
+    }
+}
